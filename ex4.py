631158c27d3d2442e258c91154f89f8e9530a8ed
williamdubgod <william_vulcano@hotmail.com>
2023-02-17 22:28:08 -0300

    exercicios API

---
+++ ex4.py
@@ -0,0 +1,16 @@
+#API domínio web
+
+import requests
+
+dominio = input("Digite o domínio desejado: ")
+url = f"https://brasilapi.com.br/api/registrobr/v1/{dominio}.com.br"
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    dados = response.json()
+    if({dados['status']} == {'AVAILABLE'}):
+        print("Domínio disponível!")
+
+if({dados['status']} != {'AVAILABLE'}):
+    print("Domínio já utilizado!")
---
+++ ex5.py
@@ -0,0 +1,5 @@
+# API Feriados
+
+import requests
+
+data = input("Digite o mês e ano que deseja para saber os feriados: ")
